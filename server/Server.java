b69728e2297c6b7cd875981e230c402d275f9045
wwww-teddy <yddet.www@gmail.com>
2024-09-09 18:30:09 -0500

    Working on server connection

--- server/Server.java
+++ server/Server.java
@@ -1,4 +1,5 @@
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,20 +11,29 @@ import java.util.Random;
 public class Server {
     private static final int port = 3840;
     private static final File words_file = new File("server/resource/words.txt");   // change to relative path according to working directory
-    private static final Random rand = new Random();
-
+    private static final Random rand = new Random();    
+    
     public static void main(String[] args) {
-        System.out.println(words_file.canRead());
-
-        try {
-            Scanner scan = new Scanner(words_file);
+        // parse text file into arraylist
+        try (Scanner scan = new Scanner(words_file)) {
             ArrayList<String> words = new ArrayList<String>();
 
-            while(scan.hasNextLine()) {
+            while (scan.hasNextLine()) {
                 words.add(scan.nextLine());
-                System.out.println(words.getLast());
             }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // initialize sockets
+        try (ServerSocket server_socket = new ServerSocket(port)) {
+            // continuously listen for new connections
+            while (true) {
+                Socket client_socket = server_socket.accept();
+                
+                new ClientHandler(client_socket).start();   // make a thread
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
