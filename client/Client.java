b6ccfbe7e39bbf705a3bb5a5d5b8700d811628f9
wwww-teddy <yddet.www@gmail.com>
2024-09-15 18:28:41 -0500

    Running tests

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,10 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("this is client code");
+
+        Socket test = new Socket("localhost", 3840);
     }
 }
