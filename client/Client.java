a1f150e1ee08e16033ad340d9b179a326b83b72d
wwww-teddy <yddet.www@gmail.com>
2024-09-15 18:59:40 -0500

    Messing with git

--- client/Client.java
+++ client/Client.java
@@ -4,7 +4,6 @@ import java.net.*;
 public class Client {
     public static void main(String[] args) throws IOException {
         System.out.println("this is client code");
-
         Socket test = new Socket("localhost", 3840);
     }
 }
